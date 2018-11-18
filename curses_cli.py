be981a40ab7dc8a668a4b7c3eb9289ca81e2e09f
Joep Schuurkes <j19sch@gmail.com>
2018-11-18 10:49:21 +0100

    more playing around with curses

--- curses_cli.py
+++ curses_cli.py
@@ -1,7 +1,12 @@
 import curses  # https://docs.python.org/3/howto/curses.html
+from curses.textpad import Textbox, rectangle
 
 # example: https://gist.github.com/claymcleod/b670285f334acd56ad1c
+# lib docs: https://docs.python.org/3/library/curses.html
 # tutorial: https://steven.codes/blog/cs10/curses-tutorial/
+
+# curses.newwin(nlines, ncols, begin_y, begin_x)
+# curses.textpad.rectangle(win, uly, ulx, lry, lrx)
 
 
 def curses_interface(stdscr):
@@ -11,14 +16,21 @@ def curses_interface(stdscr):
     stdscr.refresh()
 
     # new window
-    begin_x = 20
-    begin_y = 7
-    height = 5
-    width = 40
-    win = curses.newwin(height, width, begin_y, begin_x)
+    win = curses.newwin(1, 40, 3, 3)
     win.addstr(0, 0, "Top-right of sub-window", curses.A_BOLD)
 
     win.refresh()
+
+    editwin = curses.newwin(5, 30, 10, 3)
+    rectangle(stdscr, 9, 2, 9 + 5 + 1, 2 + 30 + 1)
+    stdscr.refresh()
+
+    box = Textbox(editwin)
+
+    # Let the user edit until Ctrl-G is struck.
+    box.edit()
+
+    message = box.gather()
 
     while True:
         c = stdscr.getch()
@@ -28,3 +40,7 @@ def curses_interface(stdscr):
 
 def main():
     curses.wrapper(curses_interface)
+
+
+if __name__ == '__main__':
+    main()
