40ec52e884f3ec9e49f8fd8bf829c630a75d2f07
Joep Schuurkes <j19sch@gmail.com>
2019-02-10 12:00:44 +0100

    moves session_parser from analyzer to parser

--- session_noter/session-analyzer.py
+++ session_noter/session-analyzer.py
@@ -6,61 +6,13 @@ from markdown_writer import MarkDownWriter
 from parser import session_parser
 
 
-def analyze_notes(notes):
-    pre_session, session, post_session = session_parser(notes)
-
-    overview = {"session_notes": notes}
-    numbers = {"session_notes": notes}
-
-    for item in pre_session:
-        if item[1] in ["tester", "charter"]:
-            overview[item[1]] = item[2]
-        elif item[1] == "duration":
-            numbers[item[1]] = item[2]
-
-    start_time = None
-    end_time = None
-
-    for item in session:
-        if item[1] == "session start":
-            start_time = datetime.strptime(item[0], '%Y-%m-%dT%H:%M:%S')
-        elif item[1] == "session end":
-            end_time = datetime.strptime(item[0], '%Y-%m-%dT%H:%M:%S')
-        elif item[1] == "bug":
-            bugs.append({"bug": item[2],
-                         "session_notes": notes})
-        elif item[1] == "issue":
-            issues.append({"issue": item[2],
-                           "session_notes": notes})
-        elif item[1] == "question":
-            questions.append({"question": item[2],
-                              "session_notes": notes})
-
-    try:
-        numbers["actual_duration"] = round((end_time - start_time).seconds / 60)
-    except TypeError:
-        numbers["actual_duration"] = 0
-
-    session_overview.append(overview)
-    session_numbers.append(numbers)
-
-    numbers.update({item[1]: item[2] for item in post_session})
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("files", nargs='*',  # ToDo: type=file, # argparse.FileType('r'),
                         help="the files you want to summarize into a report")
     args = parser.parse_args()
 
-    session_overview = []
-    session_numbers = []
-    bugs = []
-    issues = []
-    questions = []
-
-    for file in args.files:
-        analyze_notes(file)
+    session_overview, session_numbers, bugs, issues, questions = session_parser(args.files)
 
     with MarkDownWriter(f"analysis-{datetime.now().strftime('%Y%m%dT%H%M%S')}.md") as md_writer:
         col_width_default = 32
