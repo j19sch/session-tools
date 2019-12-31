a372de532b061367760039e5141cd9fe533e49a3
Joep Schuurkes <j19sch@gmail.com>
2019-12-31 15:43:43 +0100

    adds type hints and mypy type checking for noter.py

--- session_noter/core/noter.py
+++ session_noter/core/noter.py
@@ -1,20 +1,22 @@
 import csv
 import datetime
-import mss
 import os
 import pathlib
+from typing import Optional, Tuple
+
+import mss
 
 
 class Noter:
-    def __init__(self, filename, tester, charter, duration):
-        self._notes = []
+    def __init__(self, filename: str, tester: str, charter: str, duration: int):
+        self._notes: list = []
         self._filename = filename
 
         self._tester = tester
         self._charter = charter
         self._duration = duration
 
-        self._session_start = None
+        self._session_start: Optional[datetime.datetime] = None
 
     def __enter__(self):
         if self._filename is not None:
@@ -37,18 +39,18 @@ class Noter:
             self._file.close()
 
     @property
-    def duration(self):
+    def duration(self) -> Optional[int]:
         try:
             return self._duration
         except AttributeError:
             return None
 
     @property
-    def notes(self):
+    def notes(self) -> list:
         return self._notes
 
     @property
-    def session_notes(self):
+    def session_notes(self) -> list:
         return [
             note
             for note in self._notes
@@ -56,25 +58,32 @@ class Noter:
             not in ["tester", "charter", "duration", "session start", "session end"]
         ]
 
-    def start_session(self):
+    def start_session(self) -> None:
         now = datetime.datetime.now()
         self._session_start = now
         self.add_note("session start", "", now)
 
-    def end_session(self):
+    def end_session(self) -> None:
         self.add_note("session end", "")
 
-    def elapsed_seconds_and_percentage(self):
-        elapsed_seconds = (
-            datetime.datetime.now() - self._session_start
-        ).total_seconds()
-        elapsed_percentage = (
-            None if self.duration is None else elapsed_seconds / (self.duration * 60)
-        )
-
+    def elapsed_seconds_and_percentage(self) -> Tuple[Optional[float], Optional[float]]:
+        if self._session_start is None:
+            elapsed_seconds = None
+            elapsed_percentage = None
+        else:
+            elapsed_seconds = (
+                datetime.datetime.now() - self._session_start
+            ).total_seconds()
+            elapsed_percentage = (
+                None
+                if self.duration is None
+                else elapsed_seconds / (self.duration * 60)
+            )
         return elapsed_seconds, elapsed_percentage
 
-    def add_note(self, note_type, content, timestamp=None):
+    def add_note(
+        self, note_type: str, content: str, timestamp: datetime.datetime = None
+    ) -> None:
         timestamp = datetime.datetime.now() if timestamp is None else timestamp
         self._notes.append(
             {"timestamp": timestamp, "type": note_type, "content": content}
@@ -85,13 +94,13 @@ class Noter:
             )
             self._file.flush()  # flush immediately so notes are captured even on crash
 
-    def all_notes_of_type(self, note_type):
+    def all_notes_of_type(self, note_type: str) -> list:
         return [note for note in self._notes if note["type"] == note_type]
 
-    def n_latest_notes(self, number):
+    def n_latest_notes(self, number: int) -> list:
         return self._notes[-number:]
 
-    def take_screenshot(self):
+    def take_screenshot(self) -> None:
         # ToDo: two screenshots during the same second results in the 2nd file overwriting the 1st
         timestamp = datetime.datetime.now()
         filename = timestamp.strftime("%Y%m%dT%H%M%S.png")
